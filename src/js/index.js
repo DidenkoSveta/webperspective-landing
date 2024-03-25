a8dd7754a1c13ec17852d5d9a5a46d4e3eb250d0
DidenkoSveta <garpia.lana@gmail.com>
2024-03-25 08:47:13 +0700

    set up a desktop header

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,19 @@
 import '../scss/style.scss'; // Путь к файлу стилей
 
 // Импорт функций из модулей
-// import burgerMenu from './modules/burger-menu';
+import burgerMenu from './modules/burger-menu';
 // import schedule from './modules/schedule';
 // import modal from './modules/modal';
 // import scroll from './modules/scroll-to-top';
 // import contactForm from './modules/contact-form';
 // import animations from './modules/animations';
 
-// // Использование функций после полной загрузки DOM
-// document.addEventListener('DOMContentLoaded', () => {
-//   burgerMenu();
+// Использование функций после полной загрузки DOM
+document.addEventListener('DOMContentLoaded', () => {
+  burgerMenu();
 //   schedule();
 //   modal();
 //   scroll();
 //   contactForm();
 //   animations();
-// });
+});
